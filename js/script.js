d5051bc808469293538302f51eb9bef16dba9be8
Serhii120796 <128705114+Serhii120796@users.noreply.github.com>
2025-03-22 11:25:53 +0200

    Update script.js

--- js/script.js
+++ js/script.js
@@ -34,32 +34,44 @@ mdfField.addEventListener("input", calculateSum);
 workField.addEventListener("input", calculateSum);
 hangerField.addEventListener("input", calculateSum);
 
+calculateSum();
+
 function calculateSum() {
-  const length = lengthField.valueAsNumber || 0;
-  const width = widthField.valueAsNumber || 0;
-  const priceBaguette = priceBaguetteField.valueAsNumber;
-  const priceСorner = priceСornersField.valueAsNumber || 0;
-  const valuePerimeter = (length + width) * 2 / 100;
-  const valueArea = length / 100 * width / 100;
-  const valueCostBaguette1 = valuePerimeter ? valuePerimeter * priceBaguette + priceСorner : "";
+  const length = lengthField.valueAsNumber; // Початкове значення NaN
+  const width = widthField.valueAsNumber; // Початкове значення NaN
+  const priceBaguette = priceBaguetteField.valueAsNumber; // Початкове значення NaN
+  const priceСorner = priceСornersField.valueAsNumber; // Початкове значення NaN
   
-  perimeterField.textContent = valuePerimeter ? valuePerimeter.toFixed(2) : "";
-  areaField.textContent = valueArea ? valueArea.toFixed(4) : "";
-  costBaguette1.textContent = valueCostBaguette1 ? valueCostBaguette1.toFixed(2) : 0;
-  costBaguette2.textContent = valueCostBaguette1 ? (valueCostBaguette1 * 1.1).toFixed(2) : 0;
+  const perimeter = (length + width) * 2 / 100; // Початкове значення NaN
+  perimeterField.textContent = perimeter ? perimeter.toFixed(2) : "";
+  
+  const area = length / 100 * width / 100; // Початкове значення NaN
+  areaField.textContent = area ? area.toFixed(4) : "";
+  
+  const valueCostBaguette1 = perimeter && priceBaguette ? perimeter * priceBaguette + (priceСorner || 0) : 0;
+  costBaguette1.textContent = valueCostBaguette1 ? valueCostBaguette1.toFixed(2) : "";
+  const valueCostBaguette2 = valueCostBaguette1 ? valueCostBaguette1 * 1.1 : 0;
+  costBaguette2.textContent = valueCostBaguette2 ? valueCostBaguette2.toFixed(2) : "";
   priceGlassField.textContent = glassField.value;
-  costGlass1.textContent = valueArea ? (valueArea * glassField.value).toFixed(2) : "";
-  costGlass2.textContent = valueArea ? (valueArea * glassField.value * 1.1).toFixed(2) : "";
+  const valueCostGlass1 = area && glassField.value ? area * Number(glassField.value) : 0;
+  costGlass1.textContent = valueCostGlass1 ? valueCostGlass1.toFixed(2) : "";
+  const valueCostGlass2 = valueCostGlass1 ? valueCostGlass1 * 1.1 : 0;
+  costGlass2.textContent = valueCostGlass2 ? valueCostGlass2.toFixed(2) : "";
   priceMDFField.textContent = mdfField.value;
-  costMDF1.textContent = valueArea ? (valueArea * mdfField.value).toFixed(2) : "";
-  costMDF2.textContent = valueArea ? (valueArea * mdfField.value * 1.1).toFixed(2) : "";
+  const valueCostMDF1 = area && mdfField.value ? area * Number(mdfField.value) : 0;
+  costMDF1.textContent = valueCostMDF1 ? valueCostMDF1.toFixed(2) : "";
+  const valueCostMDF2 = valueCostMDF1 ? valueCostMDF1 * 1.1 : 0;
+  costMDF2.textContent = valueCostMDF2 ? valueCostMDF2.toFixed(2) : "";
   priceWorkField.textContent = workField.value;
-  costWork1.textContent = workField.value ? workField.value.toFixed(2) : "";
-  costWork2.textContent = workField ? (workField.value * 1.1).toFixed(2) : "";
+  const valueCostWork1 = workField.value ? Number(workField.value) : 0;
+  costWork1.textContent = valueCostWork1 ? valueCostWork1.toFixed(2) : "";
+  const valueCostWork2 = valueCostWork1 ? valueCostWork1 * 1.1 : 0;
+  costWork2.textContent = valueCostWork2 ? valueCostWork2.toFixed(2) : "";
   priceHangerField.textContent = hangerField.value;
-  costHanger1.textContent = hangerField ? hangerField.value.toFixed(2) : "";
-  costHanger2.textContent = hangerField ? (hangerField.value * 1.1).toFixed(2) : "";
-  together.textContent = (valueCostBaguette1 * 1.1 + valueArea * glassField.value * 1.1 + valueArea * mdfField.value * 1.1 + workField.value * 1.1 + hangerField.value * 1.1).toFixed(2);
-console.log(length);
+  const valueCostHanger1 = hangerField.value ? Number(hangerField.value) : 0;
+  costHanger1.textContent = valueCostHanger1 ? valueCostHanger1.toFixed(2) : "";
+  const valueCostHanger2 = valueCostHanger1 ? valueCostHanger1 * 1.1 : 0;
+  costHanger2.textContent = valueCostHanger2 ? valueCostHanger2.toFixed(2) : "";
+  together.textContent = ((valueCostBaguette2 || 0) + (valueCostGlass2 || 0) + (valueCostMDF2 || 0) + (valueCostWork2 || 0) + (valueCostHanger2 || 0)).toFixed(2);
 }
 
